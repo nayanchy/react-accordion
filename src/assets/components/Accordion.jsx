424eb075097f88a3eee9cd1be503722a8e7735ee
Nayan Chowdhury <nayan.1aacl@gmail.com>
2023-11-20 13:52:11 +0600

    Fixed the opened accordion not clicked on click issue

--- src/assets/components/Accordion.jsx
+++ src/assets/components/Accordion.jsx
@@ -4,19 +4,22 @@ import Item from "./Item";
 const Accordion = ({ faqs }) => {
   const [isClickedId, setIsClickedId] = useState(null);
   const handleClick = (id) => {
-    setIsClickedId(id);
+    setIsClickedId((prevId) => (prevId === id ? null : id));
   };
   return (
     <div className="accordion">
-      {faqs.map((item, i) => (
-        <Item
-          key={item.id}
-          number={i + 1}
-          item={item}
-          getClickedID={handleClick}
-          isOpen={isClickedId === item.id}
-        />
-      ))}
+      {faqs.map((item, i) => {
+        const isOpened = isClickedId === item.id;
+        return (
+          <Item
+            key={item.id}
+            number={i + 1}
+            item={item}
+            getClickedID={handleClick}
+            isOpen={isOpened}
+          />
+        );
+      })}
     </div>
   );
 };
