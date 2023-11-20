1b0cbb56c5b22af437238f2bb397309980c1ecdb
Nayan Chowdhury <nayan.1aacl@gmail.com>
2023-11-20 11:03:19 +0600

    Toggle Version Final

--- src/assets/components/Accordion.jsx
+++ src/assets/components/Accordion.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import Item from "./Item";
 
 const Accordion = ({ faqs }) => {
+  const [isClickedId, setIsClickedId] = useState(null);
+  const handleClick = (id) => {
+    setIsClickedId(id);
+  };
   return (
     <div className="accordion">
       {faqs.map((item, i) => (
-        <Item key={item.id} number={i + 1} item={item} />
+        <Item
+          key={item.id}
+          number={i + 1}
+          item={item}
+          getClickedID={handleClick}
+          isOpen={isClickedId === item.id}
+        />
       ))}
     </div>
   );
--- src/assets/components/Item.jsx
+++ src/assets/components/Item.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 
-const Item = ({ item, number }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => {
-    setIsOpen((prev) => !prev);
+const Item = ({ item, number, getClickedID, isOpen }) => {
+  const handleClick = (id) => {
+    getClickedID(id);
   };
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleClick}>
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => handleClick(item.id)}
+    >
       <span className="number">{number < 10 ? `0${number}` : number}</span>
       <span className="title">{item.title}</span>
-      <span className="icon">{isOpen ? "-" : "+"}</span>
+      <span className="icon"></span>
       {isOpen && <div className="content-box">{item.text}</div>}
     </div>
   );
